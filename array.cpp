cacd1da109999caf61a05aa40aebc727e01aa02d
Electromaxim <electromaxim@gmail.com>
2023-11-13 11:17:19 +0200

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -9,12 +9,28 @@ using namespace std;
 
 int main()
 {
-    string snippet = "message there!";
-    string * pe = &str;
+std::ofstream ofs("filename");
 
-    char message [] = snippet; // 
+    // create class instance
+    const gps_position g(0, 0, 0.0f);
 
-    cout << &str << " " << *pe << endl;
+    // save data to archive
+    {
+        boost::archive::text_oarchive oa(ofs);
+        // write class instance to archive
+        oa << g;
+    	// archive and stream closed when destructors are called
+    }
 
+    // ... some time later restore the class instance to its orginal state
+    gps_position newg;
+    {
+        // create and open an archive for input
+        std::ifstream ifs("filename");
+        boost::archive::text_iarchive ia(ifs);
+        // read class state from archive
+        ia >> newg;
+        // archive and stream closed when destructors are called
+    }
     return 0;
 }
